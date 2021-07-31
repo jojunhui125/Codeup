db4921c49dca634eef3b57dd0fc05966a1c230da
jojunhui125 <86397805+jojunhui125@users.noreply.github.com>
2021-07-31 19:38:21 +0900

    https://codeup.kr/problem.php?id=4056&rid=0

---
+++ codeup 4056.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	double n;
+	scanf("%lf", &n);
+
+	if (n <= 500)
+	{
+		printf("%.f", n * 0.7);
+	}
+	else if (n > 500 && n <= 1500)
+	{
+		printf("%d",(int)( 350 + (n -500) * 0.4));
+	}
+	else if (n > 1500 && n <= 4500)
+	{
+		printf("%d",(int) (750 + (n -1500) * 0.15));
+	}
+	else if (n > 4500 && n <= 10000)
+	{
+		printf("%d", (int)(1200 + (n - 4500) * 0.05));
+	}
+	else
+	{
+		printf("%d",(int)( 1475 + (n - 10000) * 0.02));
+	}
+}
